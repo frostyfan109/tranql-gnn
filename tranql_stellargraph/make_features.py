20acade3873b466a5a4614eef03fee8e02678d3a
frostyfan109 <urnancx@gmail.com>
2020-08-17 11:18:30 -0400

    Adds comments to various neighborhood_format options

--- tranql_stellargraph/make_features.py
+++ tranql_stellargraph/make_features.py
@@ -80,20 +80,67 @@ def format3(k_graph):
 
     return nodes
 
+
+""" For `neighborhood_format`, a variety of features can incorporated that each offer their own benefits.
+For the following explanations, `source_node` is the node that the attributes are being generated for. """
+
+"""
+This creates a dict of {target_node: multiplicity},
+where multiplicity is the number of edges going from source_node->target_node.
+E.g. given a graph CHEBI:X-[targets]->HGNC:Y,
+                   CHEBI:X-[related_to]->HGNC:Y,
+                   CHEBI:X-[literature_co-occurrence]->MONDO:Z
+The connected_nodes attribute for CHEBI:X will be {"HGNC:Y": 2, "MONDO:Z": 1}
+"""
 USE_CONNECTED_NODES = True
+
+""" This attribute simply keeps track of the types of nodes that source_node is connected to.
+Remember that none of the models consider node types inherently; they have to be encoded into
+the feature vector (StellarGraph does inherently support node types with certain algorithms,
+however it does not support a node of multiple types (e.g. [disease, named_thing, genetic_condition])
+So, for example, given the aforementioned graph:
+    CHEBI:X-[targets]->HGNC:Y<gene>,
+    CHEBI:X-[related_to]->HGNC:Y<gene>,
+    CHEBI:X-[literature_co-occurrence]->MONDO:Z<disease, genetic_condition>
+this attribute would be {"gene": 2, "disease": 1, "genetic_condition": 1}
+"""
 USE_TYPE_COUNT = False
+
+""" This attribute incorporates source_node's type attribute specifically.
+Given source_node is CHEBI:X, type=["chemical_substance"],
+this attribute would be ["chemical_substance"]
+"""
 USE_NODE_TYPE = True
+
+""" This attribute extracts the ontology from the node's id. It doesn't seem
+to do much and will probably go unnoticed.
+Given source_node is CHEBI:X, this attribute would be just "CHEBI"
+"""
 USE_ONTOLOGY = True
 
+""" ONTO section. Makes requests to the ONTO API for node information. Keep in mind
+that for each node in a graph, an ONTO request will be made for every attribute enabled.
+This means that 8k requests will be made for a graph of 4000 nodes and siblings/parents turned on.
+
+Not going to describe each ONTO attribute in detail, since they all do the same thing and are self-explanatory.
+
+It's important to note that some node types don't work with ONTO, so the feature vector should not be made up of just
+ONTO attributes. For example, an ontology for genes, such as HGNC, can't be structured as a descendant tree like ONTO
+requires for a lot of its methods. Genes simply don't have parent genes. Some things still may work though; for example,
+ONTO can still return the siblings of an HGNC gene.
+"""
 USE_ONTO_PARENTS = True
 USE_ONTO_SIBLINGS = False
 USE_ONTO_CHILDREN = False
 USE_ONTO_ANCESTORS = False
 
-NEVER = 0
-FAILED_ONTO = 1
-ALWAYS = 2
-USE_NODE_ATTRIBUTES = FAILED_ONTO
+""" As mentioned above, a problem with ONTO is that nodes of certain types will often end up with basically no 
+information for their feature vector. This was an experiment to try to fix that. It tries to incorporate the node's
+actual attributes if ONTO fails to return suitable data. """
+NEVER = 0 # Never use the node's actual attributes
+FAILED_ONTO = 1 # If ONTO fails, add the node's attributes to its feature vector
+ALWAYS = 2 # Always add the node's attributes, even if ONTO succeeds (add them to every node in the graph)
+USE_NODE_ATTRIBUTES = FAILED_ONTO # Set the behavior
 
 """ This whole thing is a mess and deserves a cleanup """
 def neighborhood_format(k_graph):
